fff1639dec99d2025c1e2363a0b80f09e97abc94
Gauri <115175115+Gauripatole11@users.noreply.github.com>
2023-09-28 00:53:57 -0700

    Initial commit

---
+++ src/Map.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Grid, Button } from "@material-ui/core";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import icon from "./constants";
+
+const zoom = 13;
+const regionCoord = [37.9799, -121.3129];
+const additionalMarkerCoord = [37.9817, -121.312];
+
+const markers = [
+  {
+    position: regionCoord,
+    category: "Makerspace",
+    name: "Makerspace Location",
+    hours: "9 AM - 5 PM",
+    phone: "123-456-7890"
+  },
+  {
+    position: additionalMarkerCoord,
+    category: "Study Rooms",
+    name: "Study Rooms Location",
+    hours: "8 AM - 6 PM",
+    phone: "987-654-3210"
+  },
+  {
+    position: [37.9789, -121.3111],
+    category: "Study Rooms",
+    name: "Another Study Room",
+    hours: "7 AM - 9 PM",
+    phone: "555-555-5555"
+  },
+  {
+    position: [37.979751, -121.309154],
+    category: "Study Rooms",
+    name: "Yet Another Study Room",
+    hours: "10 AM - 8 PM",
+    phone: "444-444-4444"
+  },
+  { position: [37.9789, -121.3111], category: "LABS", info: "Lab Location" },
+  {
+    position: [37.980156, -121.310679],
+    category: "LABS",
+    info: "Another Lab Location"
+  },
+  {
+    position: [37.980155, -121.311378],
+    category: "LABS",
+    info: "Yet Another Lab Location"
+  },
+  {
+    position: [37.976279, -121.31183],
+    category: "LABS",
+    info: "Additional Lab Location"
+  },
+  {
+    position: [37.976586, -121.314124],
+    category: "LABS",
+    info: "Yet Another LABS Location"
+  } // Added the additional LABS marker
+];
+
+function Map() {
+  const [map, setMap] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  function FlyToButton() {
+    const onClick = () => {
+      map.flyTo(regionCoord, zoom);
+    };
+
+    return <Button onClick={onClick}>Add marker on click</Button>;
+  }
+
+  // Filter markers based on the selected category
+  const filteredMarkers = selectedCategory
+    ? markers.filter((marker) => marker.category === selectedCategory)
+    : markers;
+
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={2}>
+          {/* Sidebar with category buttons */}
+          <div>
+            <h2>Categories</h2>
+            <Button onClick={() => setSelectedCategory("Makerspace")}>
+              Makerspace
+            </Button>
+            <Button onClick={() => setSelectedCategory("Study Rooms")}>
+              Study Rooms
+            </Button>
+            <Button onClick={() => setSelectedCategory("LABS")}>LABS</Button>
+            <Button onClick={() => setSelectedCategory(null)}>All</Button>
+          </div>
+        </Grid>
+        <Grid item xs={10}>
+          <MapContainer
+            center={regionCoord}
+            zoom={zoom}
+            style={{ height: "90vh" }}
+            whenCreated={setMap}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {/* Display filtered markers */}
+            {filteredMarkers.map((marker, index) => (
+              <Marker key={index} position={marker.position} icon={icon}>
+                {/* Customize the Popup content */}
+                <Popup>
+                  <div>
+                    <h3>{marker.name}</h3>
+                    <p>Category: {marker.category}</p>
+                    <p>Hours: {marker.hours}</p>
+                    <p>Phone: {marker.phone}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </Grid>
+      </Grid>
+      <FlyToButton />
+    </>
+  );
+}
+
+export default Map;
